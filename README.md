0cc3f14cea26d45f074fa9ac4b5f775ccaef362a
Chaospaul42 <chaospaul42@gmail.com>
2021-06-17 14:53:51 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# PaulSMP
-Mein cooler Discord-Bot
+<h1 align="center">Welcome to PaulSMP Bot 👋</h1>
+<p>
+  <a href="#" target="_blank">
+    <img alt="License: GPL--v3.0" src="https://img.shields.io/badge/License-GPL--v3.0-yellow.svg" />
+  </a>
+</p>
+
+> Mein cooler Discord-Bot
+
+## Author
+
+👤 **ChaosPaul__**
+
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ bot.py
@@ -0,0 +1,54 @@
+import discord
+import requests
+
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix = "!", intents = intents)
+
+# Message for Starting the Bot
+@client.event
+async def on_ready():
+    print("PaulSMP ist startklar")
+
+@client.command
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@client.command
+async def info(ctx):
+    await ctx.send('Der Bot wurde von byZero und Paul gemacht - https://byzero.xyz')
+
+@client.command
+async def website(ctx):
+    await ctx.send("Du findest meine Website unter: https://linktr.ee/Chaospaul__")
+
+@client.command
+async def twitch(ctx):
+    await ctx.send('https://www.twitch.tv/chaospaul__')
+
+@client.command
+async def owner(ctx):
+    await ctx.send('Der Offizielle Server eigentümer ist <@703592068366467102> aber sonnst die alle die den <@799732215831330837>, den <@801706703619227668> und den <@795627691817959449> rang haben')
+
+@client.command
+async def ts(ctx):
+    await ctx.send('Du findest meinen TeamSpeak-Server unter: PSMP')
+
+@client.command
+async def yt(ctx):
+    await ctx.send('https://www.youtube.com/channel/UCentT7GsT0GKKtnKOP4Ew5g')
+
+@client.command
+async def meme(ctx):
+    # Request:
+    response = requests.get('https://evergene.io/api/memes/')
+    json_response = response.json()
+    imageurl = json_response['url']
+
+    # Message
+    embed=discord.Embed(title="Here you go:")
+    embed.set_image(url=imageurl)
+    await ctx.send(embed=embed)
+
+with open('tokens/token.txt','r') as file:
+    TOKEN = file.read()
+client.run(TOKEN)
